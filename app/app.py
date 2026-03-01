eb8918a483947eae5559dcf87abaec682549e301
nithish8658 <nithishkkumar253@gmail.com>
2026-03-01 13:28:26 +0530

    Testing CI automation trigger

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Azure DevOps Kubernetes Pipeline Running updated version!"
-
+    return "CI Automation Test Successful"      
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
