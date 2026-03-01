816708b0ec1954d9e35631e46c26656bd1ed0174
nithish8658 <nithishkkumar253@gmail.com>
2026-03-01 14:44:53 +0530

    test4

--- app/app.py
+++ app/app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI Automation Test Successful 2"      
+    return "CI Automation Test Successful 3"      
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
